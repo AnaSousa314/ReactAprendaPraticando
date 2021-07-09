9ddcc1d84a9b6b060adfe0b821d3ed3c68269983
Ana.Sousa <anasousa314@outlook.com>
2021-07-09 16:07:50 -0300

    cap03/tabela-livros quinto passo acrscenta interatividade aos botoes remover de modo que um clique neles delete a linha correspondente

--- cap03/tabela-livros/src/App.js
+++ cap03/tabela-livros/src/App.js
@@ -16,11 +16,22 @@ class App extends Component {
       })
       .finally(()=>console.log("Sempre retorna"));
   }
+
+  handleRemoverLinha=(id)=>{
+    //console.log("Botão clicado");
+    //filtra todos os livros que não possuem o id recebido e atribui o resultado a variavel livros
+    const livros = this.state.livros.filter(livro=>livro.id !== id);
+    //console.log(livros);
+    this.setState({livros})
+  }
   render(){
     return (
       <table className="tabela">
        <TabelaHead/>
-       <TabelaBody livros={this.state.livros}/>
+       <TabelaBody 
+          livros={this.state.livros}
+          removerLinha={this.handleRemoverLinha}
+      />
        <TabelaFoot qdeLivros={this.state.livros.length}/>
       </table>
     );
