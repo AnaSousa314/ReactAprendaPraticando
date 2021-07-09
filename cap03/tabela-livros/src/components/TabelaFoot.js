b10252449540775c348e38d00af86994e850a8c7
Ana.Sousa <anasousa314@outlook.com>
2021-07-09 15:39:12 -0300

    cap03/tabela-livros feitos o primeiro, segundo e terceiro passos

---
+++ cap03/tabela-livros/src/components/TabelaFoot.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TabelaFoot =()=>(
+  <>
+    <tfoot>
+      <tr>
+        <td colSpan="4">Este é o Rodapé da tabela</td>
+      </tr>
+    </tfoot>
+  </>
+);
+
+export default TabelaFoot;
