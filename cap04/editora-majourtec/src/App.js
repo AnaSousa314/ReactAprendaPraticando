f8178eed79d6b016a29d1fe0a80b68d720d0678e
Ana.Sousa <anasousa314@outlook.com>
2021-07-10 15:36:57 -0300

    cap04/editora-majourtec criacao da aplicacao e alteracoes no App.js

---
+++ cap04/editora-majourtec/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <>
+      <header className="topo">Topo</header>
+      <main className="principal">Principal</main>
+      <footer className="rodape">Rodapé</footer>
+    </>
+  );
+}
+
+export default App;
